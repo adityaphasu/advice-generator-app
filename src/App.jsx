dfa3531f2b1cab161f7a1b8040d9b1324771db05
Aditya Phasu <adityaphasu919@gmail.com>
2023-03-16 21:52:26 +0530

    add syncLoader and loading state

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import { useEffect, useState } from "react";
 import patternMobile from "./assets/pattern-divider-mobile.svg";
 import patternDesktop from "./assets/pattern-divider-desktop.svg";
 import Dice from "./assets/icon-dice.svg";
+import { SyncLoader } from "react-spinners";
 const API = "	https://api.adviceslip.com/advice";
 
 function App() {
-  const [id, setId] = useState("");
   const [advice, setAdvice] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const fetchApiData = async () => {
     try {
+      setLoading(true);
+
       const response = await fetch(API);
       const json = await response.json();
-      setId(json.slip.id);
-      setAdvice(json.slip.advice);
+
+      setAdvice(json.slip);
+
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -25,8 +30,12 @@ function App() {
 
   return (
     <main className="advice-card">
-      <h1 className="advice-card__heading">Advice #{id}</h1>
-      <p className="advice-card__text">"{advice}"</p>
+      <h1 className="advice-card__heading">Advice {loading ? "#???" : <>#{advice.id}</>}</h1>
+      {loading ? (
+        <SyncLoader color="#52ffa8" margin="0.5rem" className="advice-card__loader"></SyncLoader>
+      ) : (
+        <p className="advice-card__text">"{advice.advice}"</p>
+      )}
 
       <picture>
         <source media="(min-width: 780px)" srcSet={patternDesktop} />
